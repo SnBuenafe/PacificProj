79422ec27eeaee2750041b0f26f02218174dc9f7
Tin <tinbuenafe@gmail.com>
2021-03-29 20:56:53 +0200

    created a function for bycatch dist maps

--- scripts/02d_AQM_DistMaps.R
+++ scripts/02d_AQM_DistMaps.R
@@ -1,3 +1,8 @@
+# This code was written by Tin Buenafe (2021)
+# email: tinbuenafe@gmail.com
+# Please do not distribute this code without permission.
+# There are no guarantees that this code will work perfectly.
+
 # This creates a Distribution Plots of all the bycatch species.
 # Inputs include the following:
 # 1. path: where the .rds files of the bycatch are found
@@ -8,6 +13,8 @@
 # 2. world_eez: eez in robinson's projection;
 # 3. world_sf: landmasses in robinson's projection;
 # 4. rob_pacific <- "+proj=robin +lon_0=180 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+
+bycatch_distmap <- function(path, bycatch, ...) {
 
 ####################################################################################
 ####### Defining packages needed
@@ -20,13 +27,6 @@ library(stringr)
 library(patchwork)
 library(ggfittext)
 library(sf)
-
-####################################################################################
-####### Defining generalities/inputs
-####################################################################################
-
-path = "outputs/AQM_wflow/02b_bycatch"
-bycatch = "mammal"
 
 ####################################################################################
 ####### Creating bycatch data to be plotted
@@ -56,87 +56,93 @@ if(bycatch == "turtle") {
 ####################################################################################
 ####### Creating distribution plots for the bycatch
 ####################################################################################
+  plots <- list()
+  #creating objects for ggplot
+  for (i in 1:nrow(bycatch_data)) {
+    
+    pattern <- paste0(paste0(bycatch_data$aqm_spcode[i], "_", olayer, ".rds"))
+    aqm_spcode <- list.files(path = dir, pattern = pattern, full.names = TRUE)
+    rds_file <- readRDS(aqm_spcode) %>% 
+      dplyr::mutate(new_prob = Probability/Probability)
+    
+    common_name <- bycatch_data$common[i]
+    pal_dist <- rev(brewer.pal(9, "Set3"))
+    
+    if(bycatch == "turtle") {
+      x <- i
+    } else if (bycatch == "mammal") {
+      if(i <= 9) {
+        x <- i
+      } else if(i <= 18) {
+        x <- i-9
+      } else {
+        x <- i-18
+      }
+    } else {
+      print("fail")
+    }
+    
+    p <- ggplot() +
+      geom_sf(data = rds_file, color = pal_dist[x]) +
+      geom_sf(data = world_sf, size = 0.05, fill = "grey20") +
+      geom_sf(data = world_eez, size = 1, aes(color = Boundary), fill = NA, show.legend = FALSE) +
+      scale_color_manual(values = "grey30") +
+      coord_sf(xlim = c(st_bbox(Bndry)$xmin, st_bbox(Bndry)$xmax), 
+               ylim = c(st_bbox(Bndry)$ymin, st_bbox(Bndry)$ymax),
+               expand = TRUE) +
+      labs(title = (paste0(common_name," ","distribution"))) +
+      theme_bw()
+    
+    plots[[i]] <- p
+  } 
 
-pal_dist <- rev(brewer.pal(9, "BuPu"))
-
-#creating objects for ggplot
-for (i in 1:nrow(bycatch_data)) {
+  if (bycatch == "turtle") {
+  turt_plot <- (plots[[1]]) + (plots[[2]]) + (plots[[3]]) + (plots[[4]]) + (plots[[5]]) + (plots[[6]]) +
+    plot_layout(ncol = 3, nrow = 2)
   
-  pattern <- paste0(paste0(bycatch_data$aqm_spcode[i], "_", olayer, ".rds"))
-  aqm_spcode <- list.files(path = dir, pattern = pattern, full.names = TRUE)
-  rds_file <- readRDS(aqm_spcode) %>% 
-    dplyr::mutate(new_prob = Probability/Probability)
+  turt_plot + plot_annotation(tag_levels = "i",
+                              tag_suffix = ".",
+                              title = "Distribution of Marine Turtles in the Pacific Ocean ",
+                              caption = "Data from AquaMaps (2019)") +
+    ggsave("pdfs/PacificTurtles.pdf", width = 20, height = 10, dpi = 300)
   
-  common_name <- bycatch_data$common[i]
-  
-  if(i <= 9) {
-    x <- i
-  } else if (i <= 18) {
-    x <- i - 9
-  } else if (i <= 27) {
-    x <- i - 18
-  } else {
-    print("fail")
-  }
-  
-  p <- ggplot() +
-    geom_sf(data = rds_file, color = pal_dist[x]) +
-    geom_sf(data = world_sf, size = 0.05, fill = "grey20") +
-    geom_sf(data = world_eez, size = 1, aes(color = Boundary), fill = NA, show.legend = FALSE) +
-    scale_color_manual(values = "grey30") +
-    coord_sf(xlim = c(st_bbox(Bndry)$xmin, st_bbox(Bndry)$xmax), 
-             ylim = c(st_bbox(Bndry)$ymin, st_bbox(Bndry)$ymax),
-             expand = TRUE) +
-    labs(title = (paste0(common_name," ","distribution"))) +
-    theme_bw()
-  
-  assign(paste0("p",i), p)
+  } else if(bycatch == "mammal") {
+    mam_plot1 <- (plots[[1]]) + (plots[[2]]) + (plots[[3]]) + (plots[[4]]) + (plots[[5]]) + (plots[[6]]) + (plots[[7]]) + (plots[[8]]) + (plots[[9]]) +
+      plot_layout(ncol = 3, nrow = 3)
+    
+    mam_plot1 + plot_annotation(tag_levels = "i",
+                                tag_suffix = ".",
+                                title = "Distribution of Marine Mammals in the Pacific Ocean ",
+                                subtitle = "1 of 3",
+                                caption = "Data from AquaMaps (2019)") +
+      ggsave("pdfs/PacificMammals1.pdf", width = 20, height = 10, dpi = 300)
+    
+    mam_plot2 <- (plots[[10]]) + (plots[[11]]) + (plots[[12]]) + (plots[[13]]) + (plots[[14]]) + (plots[[15]]) + (plots[[16]]) + (plots[[17]]) + (plots[[18]]) +
+      plot_layout(ncol = 3, nrow = 3)
+    
+    mam_plot2 + plot_annotation(tag_levels = "i",
+                                tag_suffix = ".",
+                                title = "Distribution of Marine Mammals in the Pacific Ocean ",
+                                subtitle = "2 of 3",
+                                caption = "Data from AquaMaps (2019)") +
+      ggsave("pdfs/PacificMammals2.pdf", width = 20, height = 10, dpi = 300)
+    
+    mam_plot3 <- (plots[[19]]) + (plots[[20]]) + (plots[[21]]) + (plots[[22]]) + (plots[[23]]) + (plots[[24]]) + (plots[[25]]) + (plots[[26]]) + (plots[[27]])
+      plot_layout(ncol = 3, nrow = 3)
+    
+    mam_plot3 + plot_annotation(tag_levels = "i",
+                                tag_suffix = ".",
+                                title = "Distribution of Marine Mammals in the Pacific Ocean ",
+                                subtitle = "3 of 3",
+                                caption = "Data from AquaMaps (2019)") +
+      ggsave("pdfs/PacificMammals3.pdf", width = 20, height = 10, dpi = 300)
+  } else {print("fail")}
+}
 }
 
 ####################################################################################
-####### Creating actual plots
+####### Running function
 ####################################################################################
 
-# arranging the plots using patchwork
-# MAMMALS
-
-mam_plot1 <- p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 +
-  plot_layout(ncol = 3, nrow = 3)
-
-mam_plot1 + plot_annotation(tag_levels = "i",
-                  tag_suffix = ".",
-                  title = "Distribution of Marine Mammals in the Pacific Ocean ",
-                  subtitle = "1 of 3",
-                  caption = "Data from AquaMaps (2019)") +
-  ggsave("pdfs/PacificMammals1.pdf", width = 20, height = 10, dpi = 300)
-
-mam_plot2 <- p10 + p11 + p12 + p13 + p14 + p15 + p16 + p17 + p18 +
-  plot_layout(ncol = 3, nrow = 3)
-
-mam_plot2 + plot_annotation(tag_levels = "i",
-                            tag_suffix = ".",
-                            title = "Distribution of Marine Mammals in the Pacific Ocean ",
-                            subtitle = "2 of 3",
-                            caption = "Data from AquaMaps (2019)") +
-  ggsave("pdfs/PacificMammals2.pdf", width = 20, height = 10, dpi = 300)
-
-mam_plot3 <- p19 + p20 + p21 + p22 + p23 + p24 + p25 + p26 + p27 +
-  plot_layout(ncol = 3, nrow = 3)
-
-mam_plot3 + plot_annotation(tag_levels = "i",
-                            tag_suffix = ".",
-                            title = "Distribution of Marine Mammals in the Pacific Ocean ",
-                            subtitle = "3 of 3",
-                            caption = "Data from AquaMaps (2019)") +
-  ggsave("pdfs/PacificMammals3.pdf", width = 20, height = 10, dpi = 300)
-
-# TURTLES 
-
-turt_plot <- p1 + p2 + p3 + p4 + p5 + p6 +
-  plot_layout(ncol = 3, nrow = 2)
-
-turt_plot + plot_annotation(tag_levels = "i",
-                            tag_suffix = ".",
-                            title = "Distribution of Marine Turtles in the Pacific Ocean ",
-                            caption = "Data from AquaMaps (2019)") +
-  ggsave("pdfs/PacificTurtles.pdf", width = 20, height = 10, dpi = 300)
+bycatch_distmap(path = "outputs/AQM_wflow/02b_bycatch",
+     bycatch = "mammal")
