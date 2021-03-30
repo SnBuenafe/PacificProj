fc8775904e13f6a9ea1d7862da9ecda6ad7f816b
Tin <tinbuenafe@gmail.com>
2021-03-30 10:44:29 +0200

    intersecting PUs with bycatch AQM
    just working with turtles

--- scripts/03a_AQM_Featuresfxn.R
+++ scripts/03a_AQM_Featuresfxn.R
@@ -1,3 +1,5 @@
+# with some modifications by Tin Buenafe, 2021 (tinbuenafe@gmail.com)
+
 # This code was written by Isaac Brito-Morales (i.britomorales@uq.edu.au)
 # Please do not distribute this code without permission.
 # NO GUARANTEES THAT CODE IS CORRECT
@@ -30,17 +32,23 @@ features_pus <- function(path, outdir, pu_shp, olayer) {
   } else if (stringr::str_detect(string = pu_shp, pattern = ".shp") == TRUE) {
     shp_PU_sf <- st_read(pu_shp)
   }
-  # If no cellsID values were assinged to the original
-  if(colnames(shp_PU_sf) == "geometry") {
-    shp_PU_sf <- mutate(shp_PU_sf, cellsID = 1:nrow(shp_PU_sf))
-  } else{shp_PU_sf}
+  
+# If no cellsID values were assinged to the original
+#  if(colnames(shp_PU_sf) == "geometry") {
+#    shp_PU_sf <- mutate(shp_PU_sf, cellsID = 1:nrow(shp_PU_sf))
+#  } else{shp_PU_sf}
+  
+  shp_PU_sf <- mutate(shp_PU_sf, cellsID = 1:nrow(shp_PU_sf))
+  
   # To add more info [not sure about this so it can be delete it]
 #  col_ns <- colnames(shp_PU_sf)
 #  col_ns[2] <- ifelse(col_ns[2] != "cellsID", "cellsID", col_ns[2])
 #  colnames(shp_PU_sf) <- col_ns
-#  shp_PU_sf <- shp_PU_sf %>%
-#    dplyr::mutate (area_km2 = as.numeric(st_area(shp_PU_sf)/1e+06))
-#  pu_min_area <- min(shp_PU_sf$area_km2)
+  
+  shp_PU_sf <- shp_PU_sf %>%
+    dplyr::mutate(area_km2 = as.numeric(st_area(shp_PU_sf)/1e+06))
+  pu_min_area <- min(shp_PU_sf$area_km2)
+  
   # Reading conservation features .rds files (AquaMaps)
   dir <- path
   pattern1 <-  c(paste0("*", ".*.rds$"), paste0("*", ".*shp$"))
--- scripts/03b_AQM_FeaturesfxnRun.R
+++ scripts/03b_AQM_FeaturesfxnRun.R
@@ -8,14 +8,28 @@
 ####################################################################################
 # Use the source argument to call the function into the R environment
 source("scripts/03a_AQM_Featuresfxn.R") 
+#running with .rds
 run01 <- features_pus(path = "outputs/AQM_wflow/02b_bycatch",
                       outdir = "outputs/AQM_wflow/03b_features/",
                       pu_shp = "inputs/rdsfiles/PacificABNJGrid_05deg.rds",
                       olayer = "surface")
 run01
 
+#running with .shp
+run02 <- features_pus(path = "outputs/AQM_wflow/02b_bycatch",
+                      outdir = "outputs/AQM_wflow/03b_features/",
+                      pu_shp = "inputs/shapefiles/PacificABNJGrid_05deg/PacificABNJGrid_05deg.shp",
+                      olayer = "surface")
+
+#creating data matrix for the total areas of the features (and their percentages)
+#total number of features: 31917 features
+#area per cell: 2667.6 km^2
+total_PU_area <- 2667.6 * 31917
+temp <- as.data.frame(run01) %>% rename(features = feature_names) %>% group_by(features) %>% summarize(total_area = sum(area_km2), percentage = (sum(area_km2)/total_PU_area)*100) 
+print(temp, n = 1000)
+
 ggplot()+
-  geom_sf(data = run01, aes(color = as.factor(feature_names))) +
-  theme_bw() +
-  ggsave("pdfs/features.pdf", width = 20, height = 10, dpi = 300)
+  geom_sf(data = run01, color = "coral3") +
+  theme_bw()# +
+#  ggsave("pdfs/features.pdf", width = 20, height = 10, dpi = 300)
 
