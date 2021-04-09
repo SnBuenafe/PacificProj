2f2881104c239c73d2a105da947ecd26321668fd
Tin <tinbuenafe@gmail.com>
2021-04-09 12:23:34 +0200

    changes

--- scripts/03b_AQM_FeaturesfxnRun.R
+++ scripts/03b_AQM_FeaturesfxnRun.R
@@ -9,14 +9,14 @@
 # Use the source argument to call the function into the R environment
 source("scripts/03a_AQM_Featuresfxn.R") 
 #running with .rds
-run01 <- features_pus(path = "outputs/AQM_wflow/02b_bycatch",
+run_a <- features_pus(path = "outputs/AQM_wflow/02b_bycatch",
                       outdir = "outputs/AQM_wflow/03b_features/",
                       pu_shp = "inputs/rdsfiles/PacificABNJGrid_05deg.rds",
                       olayer = "surface")
-run01
+run_a
 
 #running with .shp
-run02 <- features_pus(path = "outputs/AQM_wflow/02b_bycatch",
+run_b <- features_pus(path = "outputs/AQM_wflow/02b_bycatch",
                       outdir = "outputs/AQM_wflow/03b_features/",
                       pu_shp = "inputs/shapefiles/PacificABNJGrid_05deg/PacificABNJGrid_05deg.shp",
                       olayer = "surface")
@@ -25,10 +25,15 @@ run02 <- features_pus(path = "outputs/AQM_wflow/02b_bycatch",
 #total number of features: 31917 features
 #area per cell: 2667.6 km^2
 total_PU_area <- 2667.6 * 31917
-temp <- as.data.frame(run01) %>% rename(features = feature_names) %>% group_by(features) %>% summarize(total_area = sum(area_km2), percentage = (sum(area_km2)/total_PU_area)*100) 
+temp <- as.data.frame(run_a) %>% 
+  rename(features = feature_names) %>% 
+  group_by(features) %>% 
+  summarize(total_area = sum(area_km2), percentage = (sum(area_km2)/total_PU_area)*100) 
 print(temp, n = 1000)
 
+# just to test it out
 ggplot()+
-  geom_sf(data = run01, color = "coral3") +
+  geom_sf(data = run_a, aes(color = feature_names)) +
   theme_bw()# +
 #  ggsave("pdfs/features.pdf", width = 20, height = 10, dpi = 300)
+
