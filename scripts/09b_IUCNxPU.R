56a71a6ae76a29ed36d643ef627069fd1ddf89ef
Tin <tinbuenafe@gmail.com>
2021-04-22 16:57:41 +0200

    distribution plots for sea turtles (IUCN)

--- scripts/09b_IUCNxPU.R
+++ scripts/09b_IUCNxPU.R
@@ -15,7 +15,7 @@
 
 # Function is detailed in 10a_IUCNdata.R
 
-source("scripts/10a_IUCNdata.R")
+source("scripts/09a_IUCNdata.R")
 
 ############################
 # RUNNING THE CODE
@@ -23,20 +23,103 @@ source("scripts/10a_IUCNdata.R")
 
 run29 <- iucn_int(input = "inputs/rasterfiles/IUCN/data_0.shp",
                   pu_file = "inputs/rdsfiles/PacificABNJGrid_05deg.rds",
-                  outdir = "outputs/IUCN_wflow/10a_IUCNxPU/"
+                  outdir = "outputs/IUCN_wflow/09b_IUCNxPU/"
 )
 
-test <- readRDS("outputs/IUCN_wflow/10a_IUCNxPU/Lepidochelys_olivacea_IUCN.rds")
+#################################
+# PLOTTING THE DISTRIBUTIONS
+#################################
 
+# Defining generalities and objects for plotting
+library(RColorBrewer)
+library(patchwork)
+pal_dist <- rev(brewer.pal(6, "Set3"))
 world_sf <- st_read("inputs/shapefiles/PacificCenterLand/PacificCenterLand.shp") %>% 
   st_transform(crs = rob_pacific)
 
-ggplot() + 
-  geom_sf(data = test, colour = "coral3") + 
-  geom_sf(data = world_sf, fill = "grey20", colour = NA) +
-  geom_sf(data = world_eez, size = 1, aes(color = Boundary), fill = NA, show.legend = FALSE) +
-  scale_color_manual(values = "grey30") +
-  coord_sf(xlim = c(st_bbox(Bndry)$xmin, st_bbox(Bndry)$xmax), 
-           ylim = c(st_bbox(Bndry)$ymin, st_bbox(Bndry)$ymax),
-           expand = TRUE) +
-  theme_bw()
+loggerhead <- readRDS("outputs/IUCN_wflow/09b_IUCNxPU/Caretta_caretta_IUCN.rds")
+green <- readRDS("outputs/IUCN_wflow/09b_IUCNxPU/Chelonia_mydas_IUCN.rds")
+leatherback <- readRDS("outputs/IUCN_wflow/09b_IUCNxPU/Dermochelys_coriacea_IUCN.rds")
+hawksbill <- readRDS("outputs/IUCN_wflow/09b_IUCNxPU/Eretmochelys_imbricata_IUCN.rds")
+kemp <- readRDS("outputs/IUCN_wflow/09b_IUCNxPU/Lepidochelys_kempii_IUCN.rds")
+olive <- readRDS("outputs/IUCN_wflow/09b_IUCNxPU/Lepidochelys_olivacea_IUCN.rds")
+
+loggerhead_plot <- ggplot() + 
+                      geom_sf(data = loggerhead, colour = pal_dist[1]) + 
+                      geom_sf(data = world_sf, fill = "grey20", colour = NA) +
+                      geom_sf(data = world_eez, size = 1, aes(color = Boundary), fill = NA, show.legend = FALSE) +
+                      scale_color_manual(values = "grey30") +
+                      coord_sf(xlim = c(st_bbox(Bndry)$xmin, st_bbox(Bndry)$xmax), 
+                               ylim = c(st_bbox(Bndry)$ymin, st_bbox(Bndry)$ymax),
+                               expand = TRUE) +
+                      labs(title = "Loggerhead sea turtles' distribution") +
+                      theme_bw()
+loggerhead_plot
+
+green_plot <- ggplot() + 
+                  geom_sf(data = green, colour = pal_dist[2]) + 
+                  geom_sf(data = world_sf, fill = "grey20", colour = NA) +
+                  geom_sf(data = world_eez, size = 1, aes(color = Boundary), fill = NA, show.legend = FALSE) +
+                  scale_color_manual(values = "grey30") +
+                  coord_sf(xlim = c(st_bbox(Bndry)$xmin, st_bbox(Bndry)$xmax), 
+                           ylim = c(st_bbox(Bndry)$ymin, st_bbox(Bndry)$ymax),
+                           expand = TRUE) +
+                  labs(title = "Green sea turtles' distribution") +
+                  theme_bw()
+green_plot
+
+leatherback_plot <- ggplot() + 
+                        geom_sf(data = leatherback, colour = pal_dist[3]) + 
+                        geom_sf(data = world_sf, fill = "grey20", colour = NA) +
+                        geom_sf(data = world_eez, size = 1, aes(color = Boundary), fill = NA, show.legend = FALSE) +
+                        scale_color_manual(values = "grey30") +
+                    coord_sf(xlim = c(st_bbox(Bndry)$xmin, st_bbox(Bndry)$xmax), 
+                             ylim = c(st_bbox(Bndry)$ymin, st_bbox(Bndry)$ymax),
+                             expand = TRUE) +
+                    labs(title = "Leatherback sea turtles' distribution") +
+                    theme_bw()
+leatherback_plot
+
+hawksbill_plot <- ggplot() + 
+                      geom_sf(data = hawksbill, colour = pal_dist[4]) + 
+                      geom_sf(data = world_sf, fill = "grey20", colour = NA) +
+                      geom_sf(data = world_eez, size = 1, aes(color = Boundary), fill = NA, show.legend = FALSE) +
+                      scale_color_manual(values = "grey30") +
+                      coord_sf(xlim = c(st_bbox(Bndry)$xmin, st_bbox(Bndry)$xmax), 
+                               ylim = c(st_bbox(Bndry)$ymin, st_bbox(Bndry)$ymax),
+                               expand = TRUE) +
+                      labs(title = "Hawksbill sea turtles' distribution") +
+                      theme_bw()
+hawksbill_plot
+
+kemp_plot <- ggplot() + 
+                geom_sf(data = kemp, colour = pal_dist[5]) + 
+                geom_sf(data = world_sf, fill = "grey20", colour = NA) +
+                geom_sf(data = world_eez, size = 1, aes(color = Boundary), fill = NA, show.legend = FALSE) +
+                scale_color_manual(values = "grey30") +
+                coord_sf(xlim = c(st_bbox(Bndry)$xmin, st_bbox(Bndry)$xmax), 
+                         ylim = c(st_bbox(Bndry)$ymin, st_bbox(Bndry)$ymax),
+                         expand = TRUE) +
+                labs(title = "Kemp's ridley sea turtles' distribution") +
+                theme_bw()
+kemp_plot
+
+olive_plot <- ggplot() + 
+                geom_sf(data = olive, colour = pal_dist[6]) + 
+                geom_sf(data = world_sf, fill = "grey20", colour = NA) +
+                geom_sf(data = world_eez, size = 1, aes(color = Boundary), fill = NA, show.legend = FALSE) +
+                scale_color_manual(values = "grey30") +
+                coord_sf(xlim = c(st_bbox(Bndry)$xmin, st_bbox(Bndry)$xmax), 
+                         ylim = c(st_bbox(Bndry)$ymin, st_bbox(Bndry)$ymax),
+                         expand = TRUE) +
+                labs(title = "Olive ridley sea turtles' distribution") +
+                theme_bw()
+
+# removing kemp_plot because there are no planning units
+iucn_turtle_plots <- (loggerhead_plot | green_plot | leatherback_plot) / (hawksbill_plot | olive_plot) +
+  plot_annotation(tag_levels = "i",
+                  tag_suffix = ".",
+                  title = "Distribution of Sea Turtles in the Pacific Ocean ",
+                  caption = "Data from IUCN")
+iucn_turtle_plots
+ggsave("pdfs/PacificTurtles_IUCN.pdf", width = 20, height = 10, dpi = 300)
