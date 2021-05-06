75d583f1f9cb9e2bc017dcf49d6b3db8d0f9f8c4
Tin <tinbuenafe@gmail.com>
2021-05-06 20:11:19 +0200

    uppdates

--- scripts/13a_Statistics.R
+++ scripts/13a_Statistics.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(stats)
+library(grid)
+
 
 stats <- read_csv("statistics/stats.csv")
 stats
@@ -33,3 +35,56 @@ hist(stats$protected_PU)
 protected_PU_model1 <- glm(protected_PU ~ as.factor(target) + as.factor(scenario), 
                            family = poisson(link = "log"), data = stats)
 summary(protected_PU_model1)
+
+### PLOTTING
+stats_filtered <- stats %>% 
+  filter(scenario != "uninformed")
+
+ggplot(data = stats_filtered, aes(x = as.factor(target), group = scenario)) +
+  geom_line(aes(color = scenario, y = log10(cost)), size = 0.5)+
+  geom_point(aes(shape = scenario, color = scenario,y = log10(cost)), size = 3) +
+  scale_color_brewer(palette = "Reds") +
+  xlab("Target (%)") + 
+  ylab("log10(cost)") +
+  theme(legend.position = "bottom") +
+  theme_classic()
+
+log_plot <- ggplot(data = stats_filtered, aes(x = as.factor(target), group = scenario)) +
+  geom_line(aes(color = scenario, y = log10(cost)), size = 0.5)+
+  geom_point(aes(shape = scenario, color = scenario,y = log10(cost)), size = 3) +
+  scale_color_brewer(palette = "Reds") +
+  xlab("Target (%)") + 
+  ylab("log10(cost)") +
+  theme(legend.position = "bottom") +
+  theme_classic()
+log_plot
+
+area_plot <- ggplot(data = stats_filtered, aes(x = as.factor(target), group = scenario)) +
+  geom_line(aes(color = scenario, y = perc_area_protected), size = 0.5) +
+  geom_point(aes(shape = scenario, color = scenario,y = perc_area_protected), size = 3) +
+  scale_color_brewer(palette = "Blues") +
+  xlab("Target (%)") + 
+  ylab("Protected Area (%)") +
+  theme(legend.position = "bottom") +
+  theme_classic()
+area_plot
+
+protected_features <- ggplot(data = stats_filtered, aes(x = as.factor(target), group = scenario)) +
+  geom_line(aes(color = scenario, y = protected_PU), size = 0.5) +
+  geom_point(aes(shape = scenario, color = scenario, y = protected_PU), size = 3) +
+  scale_color_brewer(palette = "Purples") +
+  xlab("Target (%)") + 
+  ylab("Number of Features") +
+  theme(legend.position = "bottom") +
+  theme_classic()
+protected_features
+
+log_plot1 <- log_plot + theme(axis.text.x = element_blank(),
+                  axis.title.x = element_blank() )
+
+area_plot1 <- area_plot + theme(axis.text.x = element_blank(),
+                           axis.title.x = element_blank() )
+
+log_plot1 / area_plot1 / protected_features +
+  plot_annotation(tag_levels = 'i')
+ggsave("pdfs/stats.pdf", width = 21, height = 29.7, units = "cm")
